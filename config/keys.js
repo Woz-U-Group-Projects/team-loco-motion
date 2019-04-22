8e65bd3f1cc60273635a8d3e4024a92c60fd32ce
Paul <bucksorn@gmail.com>
2019-04-21 20:18:54 -0700

    Server connected

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: "mongodb://<team-loco-motion>:<qwerty1234>@ds145456.mlab.com:45456/love-on-rails" 
+    mongoURI: "mongodb://team-loco-motion:qwerty1234@ds145456.mlab.com:45456/love-on-rails" 
   };
