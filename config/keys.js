01cd5c52f8665b87c9d29e87a91a604fae1ec8f5
Paul <bucksorn@gmail.com>
2019-04-21 20:36:52 -0700

    Create the Register/Login endpoint and setup passport

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://team-loco-motion:qwerty1234@ds145456.mlab.com:45456/love-on-rails" 
-  };
+    mongoURI: "mongodb://team-loco-motion:qwerty1234@ds145456.mlab.com:45456/love-on-rails",
+    secretOrKey: "secret"
+};
