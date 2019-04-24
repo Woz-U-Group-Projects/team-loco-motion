c54861995f24997d3be6ca17150d9144431d9fe1
Paul <bucksorn@gmail.com>
2019-04-23 21:35:01 -0700

    Wording

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -16,8 +16,7 @@ return (
             <h4>
               <b>Hey there,</b> {user.name.split(" ")[0]}
               <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+                You are logged into Love On Rails
               </p>
             </h4>
             <button
